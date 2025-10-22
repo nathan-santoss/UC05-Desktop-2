971f082bf5b4d0fc1fe3c14ef3e5de53b6ec6515
Nathan Santos <nathan.oliver07@hotmail.com>
2025-10-21 21:38:54 -0300

    :books: atividade 1 iniciada

---
+++ aula-1/atividade-01/main.js
@@ -0,0 +1,12 @@
+import {app, BrowserWindow} from "electron"
+
+const criarJanela = () => {
+    const janela = new BrowserWindow({width: 800, height: 600})
+    janela.loadFile('./index.html')
+    janela.setMenu(null)
+}
+
+app.whenReady()
+    .then(() => {
+        criarJanela()
+    })
