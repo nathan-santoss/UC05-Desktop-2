68be0fd09cb9d5bbb2909fe1413a4d40b5c8d90e
nathan-santoss <nathan.oliver07@hotmail.com>
2025-10-23 15:49:23 -0300

    :books: junção de todas as atividades

--- aula-1/atividade-03/main.js
+++
@@ -1,9 +0,0 @@
-import { app, BrowserWindow } from "electron";
-
-const criarJanela = () => {
-    const janela = new BrowserWindow( {width: 800, height: 500})
-    janela.loadFile('./index.html')
-    janela.setMenu(null)
-}
-
-app.whenReady().then(() => criarJanela())
