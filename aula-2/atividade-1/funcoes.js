ab67680f80f8e9ca1f0ccd9e13c18494773209da
nathan-santoss <nathan.oliver07@hotmail.com>
2025-10-27 11:23:56 -0300

    :books: atividade finalizada

--- aula-2/atividade-1/funcoes.js
+++ aula-2/atividade-1/funcoes.js
@@ -1,21 +1,35 @@
 const converter = (event) => {
-    const moedaReal = document.getElementById('caixa').value
+    const moedaReal = parseFloat(document.getElementById('caixa').value)
     const selecionado = event.target.dataset.moeda
+
+    document.getElementById('resultado').style.display = 'block'
+    if(!moedaReal && moedaReal !== 0){
+        document.getElementById('result').innerHTML = `Preenchimento inválido!`
+        return
+    }
     switch(selecionado){
         case 'dolar':
+            const dolar = moedaReal/5.39
+            document.getElementById('result').innerHTML = `O valor em DÓLAR é $${dolar.toFixed(2)}`
             break
         case 'euro':
+            const euro = moedaReal/6.27
+            document.getElementById('result').innerHTML = `O valor em EURO é ${euro.toFixed(2)} €`
             break
         case 'peso':
+            const peso = moedaReal/0.003613
+            document.getElementById('result').innerHTML = `O valor em PESO ARGENTINO é $${peso.toFixed(2)} `
             break
         case 'libra':
+            const libra = moedaReal/7.17
+            document.getElementById('result').innerHTML = `O valor em LIBRA é ${libra.toFixed(2)} £`
             break
-        case 'libra':
-            break
-        default:
+        case 'franco':
+            const franco = moedaReal/6.77
+            document.getElementById('result').innerHTML = `O valor em FRANCO é ${franco.toFixed(2)} ₣ `
             break
     }
-    
+    document.getElementById('caixa').value = ''
 }
 document.querySelectorAll("button[data-moeda]").forEach(botao => {
     botao.addEventListener('click', converter)
--- aula-2/main.js
+++ aula-2/main.js
@@ -1,9 +1,17 @@
 import { app, BrowserWindow } from "electron";
 
 const criarJanela = () => {
-    const janela = new BrowserWindow({width: 800, height: 800})
+    const janela = new BrowserWindow({width: 800, height: 400, resizable: false, maximizable: false, minimizable: false})
     janela.loadFile('./index.html')
     janela.setMenu(null)
+    
+    const momento = new Date()
+    janela.once('ready-to-show', () => {
+        console.log(`========= APP INICIADO (${momento.toLocaleTimeString('pt-BR')}) =========`);
+    })
+    janela.on('closed', () => {
+        console.log(`========= APP FINALIZADO (${momento.toLocaleTimeString('pt-BR')}) =========`);
+    })
 }
 
-app.whenReady().then(() => criarJanela())
+app.whenReady().then(criarJanela)
