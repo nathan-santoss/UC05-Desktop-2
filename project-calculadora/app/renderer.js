789736f6e70a94c34039f6ab9e9c861a6b463bef
nathan-santoss <nathan.oliver07@hotmail.com>
2025-10-30 15:29:38 -0300

    :books: atividade finalizada

--- project-calculadora/app/renderer.js
+++ project-calculadora/app/renderer.js
@@ -1,2 +1,16 @@
+const captacao = () => {
+    const teclado = document.getElementById('teclado')
+    
+    teclado.addEventListener('click', (event) => {
+        const botao = event.target.textContent;
+        if(botao === 'CE'){document.getElementById('caixa').value = ''} 
+        else if(botao.trim() === '='){
+            const caixa = document.getElementById('caixa').value
+            window.api.operar(caixa)
+            document.getElementById('caixa').value = window.api.resolucao()
+        }
+        else{document.getElementById('caixa').value += botao}
+    })
+}
 
-
+window.addEventListener('load', captacao)
