3ff412325884983695a8695ec1206e8500f7b450
nathan-santoss <nathan.oliver07@hotmail.com>
2025-10-30 15:51:31 -0300

    :books: att

--- project-calculadora/src/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge } from "electron"
-
-let resultado
-contextBridge.exposeInMainWorld('api',{
-    name: 'calculator',
-    operar: (entrada) => resultado = eval(entrada),
-    resolucao: () => resultado.toFixed(2)
-})
